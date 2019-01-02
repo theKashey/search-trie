ab5b52b8098889b61416f582c49fb50ebede49b5
Anton Korzunov <thekashey@gmail.com>
2019-01-02 19:39:57 +1100

    feat: return node edges

--- _tests/smoke.spec.js
+++ _tests/smoke.spec.js
@@ -4,7 +4,9 @@ import buildTrie from '../src/index';
 
 describe('trie', () => {
   it('should bind and search', () => {
-    const trie1 = buildTrie(['a','b','test','testable']);
+    const trie1 = buildTrie(['a', 'b', 'test', 'testable', 'testable.stuff']);
+    expect(Array.from(trie1.edges.values())).to.be.deep.equal(['a', 'b', 'test', 'testable.stuff']);
+
     expect(trie1('a')).to.be.true;
     expect(trie1('b')).to.be.true;
     expect(trie1('c')).to.be.false;
@@ -17,5 +19,16 @@ describe('trie', () => {
     expect(trie1('missed word')).to.be.false;
     trie1.put('missed word')
     expect(trie1('missed word')).to.be.true;
+    trie1.put('testable.stuff.1');
+    trie1.put('testable.st');
+
+    expect(Array.from(trie1.edges.values())).to.be.deep.equal([
+      "a",
+      "b",
+      "test",
+      "missed word",
+      "testable.stuff.1",
+      "testable.st",
+    ]);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,37 +1,47 @@
-const searchTrie = (words) => {
-  const root = {};
-  const put = (node, string, position) => {
-    const c = string[position];
-    let nextNode = node[c];
-    if (!nextNode) {
-      nextNode = node[c] = {};
-    }
-    const next = position + 1;
-    if (next < string.length) {
-      put(nextNode, string, next)
-    }
-  };
+const empty = {};
 
-  const get = (node, string, position) => {
+const put = (node, string, edgeNodes, edgeDelimeter) => {
+  let nextNode;
+  for (let position = 0; position < string.length; position++) {
     const c = string[position];
-    let nextNode = node[c];
+    nextNode = node[c];
+    if (!nextNode) {
+      nextNode = node[c] = Object.create(empty);
+      if (c === edgeDelimeter) {
+        edgeNodes.delete(node)
+      }
+    }
+    node = nextNode;
+  }
+  edgeNodes.set(node, string);
+  return nextNode;
+};
+
+const get = (node, string) => {
+  let nextNode;
+  for (let position = 0; position < string.length; position++) {
+    nextNode = node[string[position]];
     if (!nextNode) {
       return false;
     }
-    const next = position + 1;
-    if (next < string.length) {
-      return get(nextNode, string, next)
-    }
-    return true;
-  };
-
-  words.forEach(word => put(root, word, 0));
-
-  function contains(word) {
-    return get(root, word, 0)
+    node = nextNode;
   }
 
-  contains.put = word => put(root, word, 0);
+  return !!nextNode;
+};
+
+const searchTrie = (words, edgeDelimeter = '.') => {
+  const root = Object.create(empty);
+  const edgeNodes = new Map();
+
+  words.forEach(word => put(root, word, edgeNodes, edgeDelimeter));
+
+  function contains(word) {
+    return get(root, word)
+  }
+
+  contains.put = word => put(root, word, edgeNodes, edgeDelimeter);
+  contains.edges = edgeNodes;
   return contains;
 };
 
